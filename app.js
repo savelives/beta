7cb0e8ea8552886eb641ae57e4b4afd311ee773f
Lucas Churchill <lucas@agtlucas.com>
2014-04-26 22:14:13 -0300

    Adding pageclass to error route

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
+      pageclass: 'error',
       title: 'Nada encontrado',
       message: err.message,
       error: err
